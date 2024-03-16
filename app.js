09dd0aadf8c61710d1bd76084bed36444c5364e6
devJanu <seralaghanjanuyan@gmail.com>
2024-03-16 09:59:31 +0530

    updated

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const transactionRoutes = require("./FinanceManagement/FinanceRoute");
 const bookingRoutes = require("./BookingManagement/BookingRoute");
 const paymentRoutes = require("./FinanceManagement/Payment/PaymentRoute");
 const transportRoutes = require("./ServiceManagement/Transport/TransportRoute");
+const photographsRoutes = require("./ServiceManagement/Photography/PhotographyRoute");
 
 const db = require("./DB/connection");
 
@@ -16,13 +17,13 @@ app.use(bodyParser.json());
 app.use("/transactions", transactionRoutes);
 app.use("/payments", paymentRoutes);
 
-//Bookings
+//Booking Management
 app.use("/bookings", bookingRoutes);
 
-//Service
+//Service Management
 app.use("/transports", transportRoutes);
 // app.use("/beautician", transportRoutes);
-// app.use("/photography", transportRoutes);
+app.use("/photography", photographsRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
